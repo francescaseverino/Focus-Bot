14298b684fe8e2c9ae0c01d7d74e2a8493faae74
nylayah <njones3@stevens.edu>
2022-04-23 01:38:11 -0400

    updated set assignment function

--- discordBot/focusbot.py
+++ discordBot/focusbot.py
@@ -83,9 +83,15 @@ async def get_assignment(ctx):
         
             embed.add_field(name = doc.id,value = x["dueDate"],inline= False)
             if x["Submissions"] == True:
-                embed.add_field(name = doc.id + " has been submitted",value = "url: "+ x["URL"],inline= False)
+                if "URL" in x:
+                    embed.add_field(name = doc.id + " has been submitted",value = "url: "+ x["URL"],inline= False)
+                else:
+                    embed.add_field(name = doc.id + " has been submitted",value = "no url",inline= False)
             else:
-                embed.add_field(name = doc.id + " has not been submitted",value = "url: "+ x["URL"],inline= False)
+                if "URL" in x:
+                    embed.add_field(name = doc.id + " has not been submitted",value = "url: "+ x["URL"],inline= False)
+                else:
+                    embed.add_field(name = doc.id + " has not been submitted",value = "no url",inline= False)
             cnt += 1
         
         await ctx.send(embed = embed)   
@@ -115,9 +121,15 @@ async def get_All_assignment(ctx):
                 embed.add_field(name = doc.id,value = x["dueDate"],inline= False)
                 cnt += 1
             if x["Submissions"] == True:
-                embed.add_field(name = doc.id + " has been submitted",value = "url: "+ x["URL"],inline= False)
+                if "URL" in x:
+                    embed.add_field(name = doc.id + " has been submitted",value = "url: "+ x["URL"],inline= False)
+                else:
+                    embed.add_field(name = doc.id + " has been submitted",value = "no url",inline= False)
             else:
-                embed.add_field(name = doc.id + " has not been submitted",value = "url: "+ x["URL"],inline= False)
+                if "URL" in x:
+                    embed.add_field(name = doc.id + " has not been submitted",value = "url: "+ x["URL"],inline= False)
+                else:
+                    embed.add_field(name = doc.id + " has not been submitted",value = "no url",inline= False)
             cnt += 1
            
         await ctx.send(embed = embed)   
@@ -232,59 +244,78 @@ async def set_getAssignmentRange(ctx):
 # add assignments not in canvas
 @bot.command()
 async def set_Assignment(ctx):
-    def check(message,context,sz):
-        return message.author == context.author and message.channel == context.channel and int(message.content) > 0 and int(message.content) < sz
+    def check(message,ctx,sz):
+        return message.author == ctx.author and message.channel == ctx.channel and int(message.content) > 0 and int(message.content) < sz
 
     courseList = []
+
     courses = db.collection("{}".format(ctx.author)).document("courses").collection("courseName").stream()
     embed=discord.Embed(title="is this assignment related to one of your canvas courses?",inline=False)
-    await context.send(embed = embed)
+    
+    await ctx.send(embed = embed)
     message = await bot.wait_for("message")
+
+
     '''adding assignment to current course'''
-    if message.content.lower() == "y" or "yes":
+    if message.content.lower()=="yes" or message.content.lower()== 'y':
         data={}
         assignmentName=""
         embed=discord.Embed(title="which course?",inline=False)
-        courseID = 1
+        
+    
         '''creates course list for user to pick from '''
+        courseID = 1
         for course in courses:
             embed.add_field(name = course.id,value = courseID,inline= False)
             courseList.append(course.id)
             courseID+=1
-        await context.send(embed = embed) 
+        await ctx.send(embed = embed) 
         message = await bot.wait_for("message")
+
         '''checking if user selection is contained in given list'''
-        while not(check(message,context,courseID)):
-            await context.send(f"try picking a number from the list! (1 to " + courseID +")")
+        while not(check(message,ctx,courseID)):
+            await ctx.send(f"try picking a number from the list! (1 to " + courseID +")")
             message = await bot.wait_for("message")
+
         '''setting course'''
         courseSelection=int(message.content)-1
+        
         '''setting assignment name'''
         embed=discord.Embed(title="what should we call this assignment?",inline=False)
-        await context.send(embed = embed)
+        await ctx.send(embed = embed)
         assignmentName=await bot.wait_for("message")
+        
         ''' setting due date and submission status'''
-        embed=discord.Embed(title="what is the due date? (click enter if you do not have one)",inline=False)
-        await context.send(embed = embed)
+        embed=discord.Embed(title="what is the due date in 'MM/DD/YYY hh:mm' format?",inline=False)
+        await ctx.send(embed = embed)
         message = await bot.wait_for("message")
-        data+={u'dueDate':u'message'}
-        data+={u'Submissions': u'false'}
-        db.collection("{}".format(ctx.author)).document("courses").collection(courseList[courseSelection]).document(u'assignmentName').set(data)
-        await contex.send("assignment added :)")
+        dt = datetime.datetime.strptime(message.content, r'%m/%d/%Y %H:%M')
+        data.update({u'dueDate':dt})
+        data.update({u'Submissions': False})
+        db.collection("{}".format(ctx.author)).document("courses").collection(courseList[courseSelection]).document(assignmentName.content).set(data)
+        await ctx.send("assignment added :)")
         return
+    
+    
     '''adding assignment to course labeled 'other' for miscellaneous assignments'''
-    if message.content.lower() == "n":
+    if message.content.lower()=="no" or message.content.lower()== 'n' :
+        data={}
+        
+        '''setting assignment name'''
         embed=discord.Embed(title="what should we call this assignment?",inline=False)
-        await context.send(embed = embed)
+        await ctx.send(embed = embed)
         assignmentName= await bot.wait_for("message")
+        
         ''' setting due date and submission status'''
-        embed=discord.Embed(title="what is the due date? (click enter if you do not have one)",inline=False)
-        await context.send(embed = embed)
+        embed=discord.Embed(title="what is the due date in 'MM/DD/YYY hh:mm' format?",inline=False)
+        await ctx.send(embed = embed)
         message = await bot.wait_for("message")
-        data+={u'dueDate':u'message'}
-        data+={u'Submissions': u'false'}
-        db.collection("{}".format(ctx.author)).document("courses").collection(u'other').document(u'assignmentName').set(data)
-        await contex.send("assignment added :) *this one will be listed in 'other'*")
+        dt = datetime.datetime.strptime(message.content, r'%m/%d/%Y %H:%M')
+        data.update({u'dueDate':dt})
+        data.update({u'Submissions': False})
+        db.collection("{}".format(ctx.author)).document("courses").collection(u'other').document(assignmentName.content).set(data)
+        db.collection("{}".format(ctx.author)).document("courses").collection(u'courseName').document(u"other").set({u'other': True})
+        await ctx.send("assignment added :) *this one will be listed in 'other'*")
         return
     
     
